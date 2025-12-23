0f3339ce937c99f3260715e37455d4195b399c29
Kehan Hansala <165448314+kehan-hansala@users.noreply.github.com>
2025-12-23 09:09:08 +0530

    navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router,Routes,Route, Link} from "react-router-dom";
+import {BrowserRouter as Router,Routes,Route,} from "react-router-dom";
 import './App.css'
 import Home from "./Componenets/Home.tsx";
 import Customer from "./Componenets/Cunstomer.tsx";
@@ -9,29 +9,45 @@ function App() {
   return (
 
       <Router>
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to= '/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to= '/customer'>customer</Link>
-                    </li>
-                    <li>
-                        <Link to= '/product'>product</Link>
-                    </li>
-                </ul>
-            </nav>
+          <div>
 
-            <hr/>
+              <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                  <div className="container-fluid">
+                      <a className="navbar-brand">
+                          <img src="https://www.liblogo.com/img-logo/br2066b571-brabus-logo-brabus-emblem-chrome-decals-by-ozim06-community-.png" alt="benz"/>
+                      </a>
+                      <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                              data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                              aria-label="Toggle navigation">
+                          <span className="navbar-toggler-icon"></span>
+                      </button>
+                      <div className="collapse navbar-collapse" id="navbarNav">
+                          <ul className="navbar-nav">
+                              <li className="nav-item">
+                                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                              </li>
+                              <li className="nav-item">
+                                  <a className="nav-link" href="#">Features</a>
+                              </li>
+                              <li className="nav-item">
+                                  <a className="nav-link" href="#">Pricing</a>
+                              </li>
+                              <li className="nav-item">
+                                  <a className="nav-link disabled" href="#"
+                                     aria-disabled="true">Disabled</a>
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+              </nav>
 
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/customer' element={<Customer/>}/>
-                <Route path='/product' element={<Product/>}/>
-            </Routes>
-        </div>
+
+              <Routes>
+                  <Route path='/' element={<Home/>}/>
+                  <Route path='/customer' element={<Customer/>}/>
+                  <Route path='/product' element={<Product/>}/>
+              </Routes>
+          </div>
       </Router>
 
 
