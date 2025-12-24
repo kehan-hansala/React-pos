60d85e9265e5aacdd68400a0f1aa0059a3e6915d
Kehan Hansala <165448314+kehan-hansala@users.noreply.github.com>
2025-12-24 06:32:36 +0530

    cards

--- src/Componenets/Crads/DefaultCrad.tsx
+++ src/Componenets/Crads/DefaultCrad.tsx
@@ -1,6 +1,13 @@
 import * as React from "react";
 
-function DefaultCard() {
+interface DefaultCardData{
+    thumbnail:string,
+    title:string,
+    description:string,
+    value:number| undefined
+}
+
+function DefaultCard(props:DefaultCardData) {
     const maxWidthStyle:React.CSSProperties={
         maxWidth: "540px"
     }
@@ -8,14 +15,13 @@ function DefaultCard() {
         <div className="card mb-3" style={maxWidthStyle}>
             <div className="row g-0">
                 <div className="col-md-4">
-                    <img src="..." className="img-fluid rounded-start" alt="..."/>
+                    <img src={props.thumbnail} className="img-fluid rounded-start" alt={props.title}/>
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small>
+                        <h5 className="card-title">{props.title}</h5>
+                        <p className="card-text">{props.description}</p>
+                        <p className="card-text"><small className="text-body-secondary">{props.value}</small>
                         </p>
                     </div>
                 </div>
