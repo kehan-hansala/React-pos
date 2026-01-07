b15645065e4c6f8479725cffec9b212807811660
Kehan Hansala <165448314+kehan-hansala@users.noreply.github.com>
2026-01-07 22:25:03 +0530

    product

--- src/Componenets/Product.tsx
+++ src/Componenets/Product.tsx
@@ -95,7 +95,7 @@ function Product() {
                             <td>#10110</td>
                             <td>nimal bandara</td>
                             <td>colombo</td>
-                            <td>25000</td>
+                            <td>28000</td>
                             <td>
                                 <button className='btn btn-outline-warning btn-sm'>Update</button>
 
